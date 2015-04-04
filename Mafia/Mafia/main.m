5c274861ca35484c2232df3fedbece9b48392efc
Podze Oleksandr <dasdeg@gmail.com>
2015-04-04 11:14:10 +0300

    created project

---
+++ Mafia/Mafia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mafia
+//
+//  Created by Bionic University on 4/4/15.
+//  Copyright (c) 2015 bionic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
